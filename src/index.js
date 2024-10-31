1f93c63b4321279db96bf6800927e5fe452c3e4d
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-10-31 10:09:31 +0100

    chore(setup): initialize template 🚀

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Conexión a MongoDB
+mongoose
+  .connect(process.env.MONGOURL, {})
+  .then(() => console.log("Conectado a MongoDB"))
+  .catch((err) => console.error("Error de conexión a MongoDB:", err));
+
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("API funcionando correctamente");
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
