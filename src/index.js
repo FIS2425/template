9ff1ea6ab404167aeab0cab8f49da5f0fdc35775
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2024-11-01 12:37:19 +0100

    fix(db): update mongo connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
 import swaggerUI from 'swagger-ui-express';
 import YAML from 'yamljs';
+import { connectDB } from './config/database.js';
 
 const swaggerDocument = YAML.load('./openapi.yaml');
 
@@ -10,17 +10,9 @@ const swaggerDocument = YAML.load('./openapi.yaml');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Conexión a MongoDB
-mongoose
-  .connect(process.env.MONGOURL, {})
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch((err) => console.error('Error de conexión a MongoDB:', err));
-
-// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('API funcionando correctamente');
 });
@@ -28,7 +20,17 @@ app.get('/', (req, res) => {
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en http://localhost:${PORT}/docs`);
+    });
+  } catch (error) {
+    console.error('Error al iniciar el servidor:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
